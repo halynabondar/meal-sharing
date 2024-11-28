5c870fd7abeed11c6c5734546840bb02da5bfdef
Halyna Bondar <galya.marusyak@gmail.com>
2024-11-28 16:10:21 +0100

    Styles changes

--- app/src/frontend/components/Reviews.jsx
+++ app/src/frontend/components/Reviews.jsx
@@ -1,10 +1,10 @@
 import styles from "./Reviews.module.css";
 import React, {useEffect, useState} from "react";
 import Review from "./Review.jsx";
-import {Swiper, SwiperSlide} from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import {Grid, Pagination} from 'swiper/modules';
+import {Pagination} from 'swiper/modules';
 
 export default function Reviews() {
     const [reviews, setReviews] = useState([]);
@@ -29,15 +29,15 @@ export default function Reviews() {
                 <div className={styles.swiperItem}>
                     {reviews.length > 0 ? (
                         <Swiper
-                            slidesPerView={3}
-                            spaceBetween={10}
-                            grabCursor={true}
-                            centeredSlides={false}
+                            slidesPerView={2}
+                            spaceBetween={30}
+                            loop={true}
                             pagination={{
                                 clickable: true,
                             }}
-                            modules={[Grid, Pagination]}
-                            className={styles.mySwiper}
+                            grabCursor={true}
+                            modules={[Pagination]}
+                            className="mySwiper"
                         >
                             {reviews.map((review, id) => (
                                 <SwiperSlide key={id}>
