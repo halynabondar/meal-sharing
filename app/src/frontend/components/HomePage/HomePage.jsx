5dbe1edb70254dc4f8ec421140fe19c768c998f3
Halyna Bondar <galya.marusyak@gmail.com>
2024-12-02 22:43:27 +0100

    Button added

--- app/src/frontend/components/HomePage/HomePage.jsx
+++ app/src/frontend/components/HomePage/HomePage.jsx
@@ -4,8 +4,14 @@ import styles from './HomePage.module.css';
 import Navbar from "../Navbar.jsx";
 import Footer from "../Footer/Footer.jsx";
 import HomeMealsSwiper from "./HomeMealsSwiper.jsx";
+import {useNavigate} from "react-router-dom";
 
 function HomePage() {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/meals');
+    };
 
     const backgroundStyle = {
         backgroundImage: 'url(/background14.webp)',
@@ -24,10 +30,13 @@ function HomePage() {
                     {/*<h1 className={styles.title}>We are sharing food</h1>*/}
                     <p className={styles.text}>Bringing people together, one meal at a time – share flavors, create
                         memories, and feel the warmth of community.</p>
+                    <div className={styles.containerBtn}>
+                        <button className={styles.homeBtn} onClick={handleClick}>Get started</button>
+                    </div>
                 </section>
             </div>
-            <HomeMealsSwiper />
-            <Footer />
+            <HomeMealsSwiper/>
+            <Footer/>
         </>
     );
 }
