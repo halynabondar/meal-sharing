fa019ec00010eec0a2228c1bb814db7311150a0a
Halyna Bondar <galya.marusyak@gmail.com>
2024-12-03 10:41:47 +0100

    About Us page has been updated

--- app/src/frontend/components/AboutUs/AboutUs.jsx
+++ app/src/frontend/components/AboutUs/AboutUs.jsx
@@ -13,12 +13,12 @@ export default function AboutUs() {
     };
 
     const backgroundAboutUs = {
-        backgroundImage: 'url(/public/background_about.jpg)',
-        backgroundSize: '40%',
+        backgroundImage: 'linear-gradient(to bottom right, rgba(255, 255, 255, 0.8) 20%, rgba(255, 255, 255, 0) 50%), url(/public/background4.webp)',
+        backgroundSize: '100%',
         backgroundRepeat: 'no-repeat',
-        marginTop: '20px',
-        backgroundPosition: 'right',
-        height: '100%',
+        marginTop: '50px',
+        backgroundPosition: 'top',
+        height: '800px',
         width: '100%'
     };
     return (
@@ -26,14 +26,9 @@ export default function AboutUs() {
             <Navbar/>
             <div style={backgroundAboutUs}>
                 <section className={styles.container}>
-                    <h1 className={styles.title}>About Us</h1>
                     <p className={styles.text}>We believe that the best connections are made over a
                         shared meal. Our mission is to bring people together by creating a space where anyone can enjoy
-                        good food and good company. Whether you're a passionate cook or someone eager to explore new
-                        flavors, we invite you to be part of our community.</p>
-                    <p className={styles.text}>We aim to celebrate diversity through food, honor different cultures, and make every meal a
-                        unique experience. By opening our tables to new faces, we build lasting friendships, share
-                        stories, and create memories that warm our hearts long after the meal is over.</p>
+                        good food and good company.</p>
                     <p className={styles.text}>Join us as we celebrate the joy of sharing, one plate at a time.</p>
                     <button className={styles.btn} onClick={handleFindMeal}>Find meal</button>
                 </section>
